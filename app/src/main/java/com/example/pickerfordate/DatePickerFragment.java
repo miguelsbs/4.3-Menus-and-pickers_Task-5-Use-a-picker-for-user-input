6ef02d62ef989eaad3f2ee5067df69830f769dba
Miki <miguelsanblassanchez@gmail.com>
2019-03-03 16:23:14 +0100

    4.3:Menus_and_pickers_Task_5:Use a picker for user input

---
+++ app/src/main/java/com/example/pickerfordate/DatePickerFragment.java
@@ -0,0 +1,43 @@
+package com.example.pickerfordate;
+
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+
+    public DatePickerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        MainActivity activity = (MainActivity) getActivity();
+        activity.processDatePickerResult(year, month, day);
+    }
+}
